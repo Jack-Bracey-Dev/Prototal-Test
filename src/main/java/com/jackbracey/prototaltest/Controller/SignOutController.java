2db07c38d2ff32e899a0cc8ae33847a0a5a97c4e
Jack Bracey <jack.bracey@hotmail.co.uk>
2024-03-06 15:57:13 +0000

    Implemented logout
    Still needs some cleanup, has a lot of repeated code

--- src/main/java/com/jackbracey/prototaltest/Controller/SignOutController.java
+++ src/main/java/com/jackbracey/prototaltest/Controller/SignOutController.java
@@ -1,16 +1,39 @@
 package com.jackbracey.prototaltest.Controller;
 
+import com.jackbracey.prototaltest.Security.JwtSessions;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.StringTokenizer;
 
 @RestController
 @RequestMapping("sign-out")
 public class SignOutController {
 
+    @Autowired
+    private JwtSessions jwtSessions;
+
     @PostMapping
-    public String test() {
-        return "Hello World!";
+    public ResponseEntity<?> signOut(HttpServletRequest request) {
+        String auth = request.getHeader("Authorization");
+        SecurityContextHolder.getContext().setAuthentication(null);
+        StringTokenizer tokenizer = new StringTokenizer(auth);
+
+        if (tokenizer.hasMoreTokens()) {
+            String authType = tokenizer.nextToken();
+
+            if (authType.equalsIgnoreCase("Bearer")) {
+                String bearer = auth.replace("Bearer", "").trim();
+                jwtSessions.invalidate(bearer);
+            }
+        }
+
+        return ResponseEntity.ok("Logged out");
     }
 
 }
--- src/main/java/com/jackbracey/prototaltest/Security/SecurityConfig.java
+++ src/main/java/com/jackbracey/prototaltest/Security/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.jackbracey.prototaltest.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -29,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                /*.csrf(AbstractHttpConfigurer::disable)*/
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.anyRequest().permitAll()
                 )
